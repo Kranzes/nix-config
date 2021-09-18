da8e7fc9c4532ef0f35096edf2fee1a8a817c898
Ilan Joselevich <personal@ilanjoselevich.com>
2021-09-18 21:14:25 +0300

    neovim: use lua instead of vimscript, also add nvim-tree

--- home/editors.nix
+++ home/editors.nix
@@ -13,24 +13,47 @@
       colorizer
       lexima-vim
       nvim-lspconfig
+      nvim-tree-lua
     ];
     extraPackages = [ pkgs.rnix-lsp ];
     extraConfig = ''
-      colorscheme nord
-      let g:lightline = { "colorscheme": "nord" }
-      set hidden
-      set noshowmode
-      set number
-      set mouse=a
-
       lua << EOF
-      require'lspconfig'.rnix.setup{}
+      -- enable nord theme
+      require('nord').set()
 
+      -- enable rnix via lspconfig
+      require'lspconfig'.rnix.setup{}
+      
+      -- basic vim settings
+      vim.o.number = true
+      vim.o.mouse = 'a'
+      vim.o.hidden = true
+      vim.cmd 'set noshowmode'
+
+      -- nvim-tree
+      vim.api.nvim_set_keymap( 'n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true})
+      vim.g.nvim_tree_hijack_netrw = 0
+      vim.g.nvim_tree_disable_netrw = 0
+      vim.g.nvim_tree_show_icons = {
+        git = 0,
+        folders = 0,
+        files = 0,
+        folder_arrows = 0
+      }
+
+      -- enable nord borders
+      vim.g.nord_borders = true
+
+      -- set lightline theme to nord
+      vim.g.lightline = { colorscheme = 'nord' }
+
+      -- copy to system clipboard
       vim.api.nvim_set_keymap( 'v', '<F12>', '"+y', {noremap = true})
       vim.api.nvim_set_keymap( 'n', '<F12>', ':%+y<CR>', {noremap = true})
-      EOF
 
-      autocmd BufWritePre *.nix lua vim.lsp.buf.formatting_sync(nil, nil)
+      -- run nixpkgs-fmt on save
+      vim.cmd 'autocmd BufWritePre *.nix lua vim.lsp.buf.formatting_sync(nil, nil)'
+      EOF
     '';
   };
 
