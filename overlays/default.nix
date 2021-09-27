54aeeb48becacc63cf7ca82b3a881ea597af1a57
Ilan Joselevich <personal@ilanjoselevich.com>
2021-09-28 00:18:52 +0300

    Revert "nix: patch nix to avoid isMacho error"
    This reverts commit b30ec79726d03a4d5a450eabdae88e66e658e810.

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,6 +3,4 @@
 final: prev: {
   bspswallow = prev.callPackage ./bspswallow { };
   rofi-mpd = prev.callPackage ./rofi-mpd { };
-  nixUnstable = prev.nixUnstable.override { patches = [ ../patches/unset-is-macho.patch ]; };
-  nix-direnv = prev.nix-direnv.override { inherit (final) nixUnstable; };
 }
