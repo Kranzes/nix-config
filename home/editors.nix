2ff6588d59b67c1126eb44957266787afc635ae3
Ilan Joselevich <personal@ilanjoselevich.com>
2021-11-28 21:27:34 +0200

    home/editors: cmp init

--- home/editors.nix
+++ home/editors.nix
@@ -18,6 +18,9 @@
       nvim-treesitter
       telescope-nvim
       telescope-fzy-native-nvim
+      nvim-cmp
+      cmp-nvim-lsp
+      cmp-buffer
     ];
     extraPackages = with pkgs; [ rnix-lsp gcc ];
     extraConfig = ''
@@ -48,6 +51,38 @@
         indent = {
           enable = true,
         },  
+      }
+
+      -- cmp
+      local cmp = require("cmp")
+      cmp.setup {
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+        },
+        formatting = {
+          format = function(entry, vim_item)
+            vim_item.menu = ({
+              buffer = "[Buffer]",
+              nvim_lsp = "[LSP]",
+            })[entry.source.name]
+            return vim_item
+          end
+        },
+        mapping = {
+          ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.close(),
+          ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          })
+        },        
       }
 
       -- telescope
