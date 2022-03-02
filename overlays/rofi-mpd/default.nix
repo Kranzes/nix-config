737908de34fa8ddbd756b09aef52a76da3cf04e2
Ilan Joselevich <personal@ilanjoselevich.com>
2022-03-02 15:59:04 +0200

    overlays: stdenv -> writeShellApplication

--- overlays/rofi-mpd/default.nix
+++ overlays/rofi-mpd/default.nix
@@ -1,20 +1,7 @@
-{ lib, stdenv, rofi, mpc_cli }:
+{ writeShellApplication, rofi, mpc_cli }:
 
-stdenv.mkDerivation rec {
-  pname = "rofi-mpd";
-  version = "unstable-2021-09-15";
-
-  src = ./.;
-
-  dontBuild = true;
-  dontConfigure = true;
-
-  buildInputs = [ rofi mpc_cli ];
-
-  installPhase = "install -D $src/${pname} $out/bin/${pname}";
-
-  meta = with lib; {
-    description = "A rofi menu for interacting with MPD written in Bash";
-    platforms = platforms.unix;
-  };
+writeShellApplication {
+  name = "rofi-mpd";
+  runtimeInputs = [ rofi mpc_cli ];
+  text = (builtins.readFile ./rofi-mpd);
 }
