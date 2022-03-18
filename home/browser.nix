d3bea95916401c7e4f9158f403d01800d0f894d6
Ilan Joselevich <personal@ilanjoselevich.com>
2022-03-18 21:23:25 +0200

    home/browser: changer useragent to stop extension breakage

--- home/browser.nix
+++ home/browser.nix
@@ -80,7 +80,7 @@
         "toolkit.telemetry.shutdownPingSender.enabled" = false;
         "toolkit.telemetry.unified" = false;
         "toolkit.telemetry.updatePing.enabled" = false;
-        "general.useragent.override" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36";
+        "general.useragent.override" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0";
       };
     };
   };
