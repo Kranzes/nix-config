a90c067884eaa764afa9ac09354ddd0db293c19a
Ilan Joselevich <personal@ilanjoselevich.com>
2022-03-28 17:26:05 +0300

    hosts/pongo/server-stuff: cleanup nextcloud config

--- hosts/pongo/server-stuff/nextcloud.nix
+++ hosts/pongo/server-stuff/nextcloud.nix
@@ -1,80 +1,45 @@
 { config, pkgs, ... }:
-{
 
+{
   services.nextcloud = {
     enable = true;
-    hostName = "cloud.ilanjoselevich.com";
-    ## Enable built-in virtual host management
-    ## Takes care of somewhat complicated setup
-    ## See here: https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/web-apps/nextcloud.nix#L529
-
-    # Use HTTPS for links
-    https = true;
-
     package = pkgs.nextcloud23;
-
-    logLevel = 0;
-
-    # Auto-update Nextcloud Apps
+    hostName = "cloud.ilanjoselevich.com";
+    nginx.recommendedHttpHeaders = true;
+    https = true;
     autoUpdateApps.enable = true;
-
-    maxUploadSize = "5G";
-
     config = {
-
-      # Further forces Nextcloud to use HTTPS
-      overwriteProtocol = "https";
-
-      # Nextcloud PostegreSQL database configuration, recommended over using SQLite
       dbtype = "pgsql";
-      dbuser = "nextcloud";
-      dbhost = "/run/postgresql"; # nextcloud will add /.s.PGSQL.5432 by itself
-      dbname = "nextcloud";
+      dbhost = "/run/postgresql";
       dbpassFile = "/var/nextcloud-db-pass";
-
       adminpassFile = "/var/nextcloud-admin-root-pass";
       adminuser = "admin-root";
-
       defaultPhoneRegion = "IL";
-
     };
   };
 
-
-
-
   services.postgresql = {
     enable = true;
-
-    # Ensure the database, user, and permissions always exist
-    ensureDatabases = [ "nextcloud" ];
-    ensureUsers = [
-      {
-        name = "nextcloud";
-        ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
-      }
-    ];
+    ensureDatabases = [ "${config.services.nextcloud.config.dbname}" ];
+    ensureUsers = [{
+      name = "${config.services.nextcloud.config.dbuser}";
+      ensurePermissions."DATABASE ${config.services.nextcloud.config.dbname}" = "ALL PRIVILEGES";
+    }];
   };
 
+  # Ensure nextcloud does not start before its database
   systemd.services."nextcloud-setup" = {
     requires = [ "postgresql.service" ];
     after = [ "postgresql.service" ];
   };
 
-
-  security.acme.certs = {
-    "cloud.ilanjoselevich.com".email = "personal@ilanjoselevich.com";
+  services.nginx.virtualHosts."${config.services.nextcloud.hostName}" = {
+    forceSSL = true;
+    enableACME = true;
   };
 
-  services.nginx.virtualHosts = {
-    "cloud.ilanjoselevich.com" = {
-      forceSSL = true;
-      enableACME = true;
-      kTLS = true;
-    };
+  security.acme = {
+    acceptTerms = true;
+    certs."${config.services.nextcloud.hostName}".email = "personal@ilanjoselevich.com";
   };
-
 }
-
-
-
