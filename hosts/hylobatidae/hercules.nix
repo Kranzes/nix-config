e7003e5cfa2dbe04264b43477359a804195da63f
Ilan Joselevich <personal@ilanjoselevich.com>
2022-06-24 01:48:55 +0300

    hosts: cleanup

--- hosts/hylobatidae/hercules.nix
+++
@@ -1,6 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  services.hercules-ci-agent.enable = true;
-}
-
--- hosts/pongo/server-stuff.nix
+++ hosts/pongo/server-stuff.nix
@@ -5,12 +5,9 @@
     ./server-stuff/nginx.nix
     ./server-stuff/acme.nix
     ./server-stuff/nextcloud.nix
-    #./server-stuff/wireguard.nix
-    #./server-stuff/searx.nix
     ./server-stuff/netdata.nix
     ./server-stuff/jellyfin.nix
     ./server-stuff/gitea.nix
-    #./server-stuff/main-site.nix
   ];
 
 }
