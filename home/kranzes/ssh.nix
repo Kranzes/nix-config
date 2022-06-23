d6d96f8b58805f9662d9338eb31ce3fa9618a08c
Ilan Joselevich <personal@ilanjoselevich.com>
2022-06-24 01:49:13 +0300

    home/kranzes/ssh: add platonic github

--- home/kranzes/ssh.nix
+++ home/kranzes/ssh.nix
@@ -5,6 +5,8 @@
     hashKnownHosts = true;
     matchBlocks =
       let minions = { user = "deploy"; identityFile = "~/.ssh/id_deploy"; }; in
-      lib.genAttrs [ "oracle-amd" "oracle-amd2" "oracle-ampere" "vultr" ] (_: minions);
+      lib.genAttrs [ "oracle-amd" "oracle-amd2" "oracle-ampere" "vultr" ] (_: minions)
+      //
+      { "git.ts.platonic.systems".identityFile = "~/.ssh/id_gitlab"; };
   };
 }
