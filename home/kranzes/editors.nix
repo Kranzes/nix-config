1878d27e809797e43d9626f9db16ac287764dcf2
Ilan Joselevich <personal@ilanjoselevich.com>
2022-07-17 21:16:42 +0300

    home/kranzes/editors: optimizations

--- home/kranzes/editors.nix
+++ home/kranzes/editors.nix
@@ -25,7 +25,7 @@
       cmp-treesitter
       cmp-spell
     ];
-    extraPackages = with pkgs; [ rnix-lsp gcc ripgrep fd ];
+    extraPackages = with pkgs; [ ripgrep ];
     extraConfig = ''
       lua << EOF
       -- enable nord theme
@@ -34,8 +34,12 @@
       -- enable colorizer
       require'colorizer'.setup()
 
-      -- enable rnix via lspconfig
-      require'lspconfig'.rnix.setup{}
+      -- enable rnin via lspconfig
+      require'lspconfig'.rnix.setup{
+        cmd = { "${pkgs.rnix-lsp}/bin/rnix-lsp" }
+      }
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, noremap)
+      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, noremap)
 
       -- autopairs
       require('nvim-autopairs').setup{}
@@ -86,14 +90,11 @@
         mapping = {
           ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
           ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-          ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.close(),
+          ['<C-e>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
+            behavior = cmp.ConfirmBehavior.Insert,
             select = true,
           })
         },        
@@ -101,15 +102,15 @@
 
       -- telescope
       require('telescope').load_extension('fzf')
-      vim.api.nvim_set_keymap('n', '<Leader>ff', '<cmd>Telescope find_files<CR>', {noremap = true})
-      vim.api.nvim_set_keymap('n', '<Leader>fg', '<cmd>Telescope live_grep<CR>', {noremap = true})
+      vim.keymap.set('n', '<Leader>ff', '<cmd>Telescope find_files<CR>', noremap)
+      vim.keymap.set('n', '<Leader>fg', '<cmd>Telescope live_grep<CR>', noremap)
 
       -- set lightline theme to nord
       vim.g.lightline = { colorscheme = 'nord' }
 
       -- copy to system clipboard
-      vim.api.nvim_set_keymap( 'v', '<F12>', '"+y', {noremap = true})
-      vim.api.nvim_set_keymap( 'n', '<F12>', ':%+y<CR>', {noremap = true})
+      vim.keymap.set('v', '<F12>', '"+y', noremap, silent)
+      vim.keymap.set('n', '<F12>', ':%+y<CR>', noremap, silent)
 
       -- run nixpkgs-fmt on save
       vim.cmd 'autocmd BufWritePre *.nix lua vim.lsp.buf.formatting_sync(nil, nil)'
