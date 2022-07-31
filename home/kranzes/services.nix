2aaa158477bb4f6d332b874642373ed05ba76a40
Ilan Joselevich <personal@ilanjoselevich.com>
2022-07-31 13:45:57 +0300

    home/kranzes/services: yubikey-touch-detector init

--- home/kranzes/services.nix
+++ home/kranzes/services.nix
@@ -6,5 +6,29 @@
     nextcloud-client.startInBackground = true;
     network-manager-applet.enable = true;
   };
+
+  systemd.user.sockets.yubikey-touch-detector = {
+    Unit.Description = "Unix socket activation for YubiKey touch detector service";
+    Socket = {
+      ListenStream = "%t/yubikey-touch-detector.socket";
+      RemoveOnStop = true;
+    };
+    Install.WantedBy = [ "sockets.target" ];
+  };
+
+  systemd.user.services.yubikey-touch-detector = {
+    Unit = {
+      Description = "Detects when your YubiKey is waiting for a touch";
+      Requires = "yubikey-touch-detector.socket";
+    };
+    Service = {
+      ExecStart = "${pkgs.yubikey-touch-detector}/bin/yubikey-touch-detector --libnotify";
+      EnvironmentFile = "-%E/yubikey-touch-detector/service.conf";
+    };
+    Install = {
+      Also = "yubikey-touch-detector.socket";
+      WantedBy = [ "default.target" ];
+    };
+  };
 }
 
