e4e0d662055094fb5638a4a1fcadc8dd5e0c9abd
Ilan Joselevich <personal@ilanjoselevich.com>
2022-10-17 19:48:41 +0300

    home/kranzes/editors: lsp fix

--- home/kranzes/editors.nix
+++ home/kranzes/editors.nix
@@ -84,7 +84,7 @@
       local servers = { 'nil_ls' }
       for _, lsp in ipairs(servers) do
         require('lspconfig')[lsp].setup {
-          capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+          capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
         }
       end
 
