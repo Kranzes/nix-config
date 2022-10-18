4bbc731dbcfb972512c0093ccce4113ac769d21b
Ilan Joselevich <personal@ilanjoselevich.com>
2022-10-18 09:25:56 +0000

    hosts/pan: reinstall with LUKS

--- hosts/pan/hardware-configuration.nix
+++ hosts/pan/hardware-configuration.nix
@@ -14,20 +14,20 @@
   boot.kernelParams = [ "i915.fastboot=1" "intel_iommu=on" ];
   boot.extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
 
-  fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/69a29725-3c32-42e9-9cb9-438fb8026de1";
-      fsType = "xfs";
-    };
+  boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/cead3453-145b-49bc-803f-0ba39ec9d9b2";
 
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/4E14-246A";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/8fe2fbaf-ef3c-4ef3-8097-8d0c5eeda183";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/E6DD-D259";
       fsType = "vfat";
     };
+  };
 
-  swapDevices =
-    [{ device = "/dev/disk/by-uuid/de4c7929-ed56-4d50-a86e-3ec060dba4d5"; }];
+  zramSwap.enable = true;
 
   powerManagement.cpuFreqGovernor = "conservative";
 
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   systems = {
     pongo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZiUguDNKZlxJpDmuN4Z2AAK9iH9oQLqMQpvo7RZ4q+";
-    pan = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL5BXlGWJin8AZGAaF6evxmPpACuEJdR/+TZmhttBwX7";
+    pan = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGDbBEzvhMeRzkGHpV+rd8Jq8O3hHBQYQhuzpvep08Ux";
     vultr = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII+3eMMIFUDkj7J6r1askbB/vSQaLmhb0A661PYAjWsV";
   };
   users = {
