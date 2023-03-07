e6b6dbf1f3c61aa744ce5138fb91f8b6d4acfee0
Ilan Joselevich <personal@ilanjoselevich.com>
2023-03-08 00:09:30 +0200

    profiles/tailscale: make DNS work after reboot (logout)

--- profiles/tailscale.nix
+++ profiles/tailscale.nix
@@ -1,10 +1,11 @@
-{ pkgs, config, inputs, ... }:
+{ pkgs, lib, config, inputs, ... }:
 
 {
   services.tailscale.enable = true;
 
   systemd.services.tailscaled = {
     restartIfChanged = false;
+    requires = [ "tailscaled-autoconnect.service" ];
     serviceConfig.ExecStart = [
       ""
       "${config.services.tailscale.package}/bin/tailscaled --state=mem: --port $PORT $FLAGS"
@@ -18,21 +19,25 @@
     after = [ "tailscaled.service" ];
     requires = [ "tailscaled.service" ];
     wantedBy = [ "multi-user.target" ];
-    serviceConfig.Type = "oneshot";
-    script = ''
-      # wait for tailscaled to settle
-      sleep 2
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = lib.getExe (pkgs.writeShellScriptBin "tailscale-up" ''
+        # wait for tailscaled to settle
+        sleep 2
 
-      # check if we are already authenticated to tailscale
-      status="$(${config.services.tailscale.package}/bin/tailscale status -json | ${pkgs.jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
+        # check if we are already authenticated to tailscale
+        status="$(${lib.getExe config.services.tailscale.package} status -json | ${pkgs.jq}/bin/jq -r .BackendState)"
+        if [ $status = "Running" ]; then # if so, then do nothing
+          exit 0
+        fi
 
-      # otherwise authenticate with tailscale
-      ${config.services.tailscale.package}/bin/tailscale up \
-        --authkey file:${config.age.secrets.tailscaleAuthKey.path} \
-        --hostname=${config.networking.hostName}
-    '';
+        # otherwise authenticate with tailscale
+        ${config.services.tailscale.package}/bin/tailscale up \
+          --authkey file:${config.age.secrets.tailscaleAuthKey.path} \
+          --hostname=${config.networking.hostName}
+      '');
+      ExecStop = lib.getExe (pkgs.writeShellScriptBin "tailscale-logout" "${lib.getExe config.services.tailscale.package} logout || true");
+    };
   };
 }
