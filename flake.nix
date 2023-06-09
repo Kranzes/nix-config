6b028dfbbb7faf4fc89a3fdf1092d1430c86ecb3
Ilan Joselevich <personal@ilanjoselevich.com>
2023-06-09 19:18:24 +0300

    flake.nix: nixos-unstable -> nixpkgs-unstable-small

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My nix flake configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     flake-parts = { url = "github:hercules-ci/flake-parts"; inputs.nixpkgs-lib.follows = "nixpkgs"; };
     home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
     neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
