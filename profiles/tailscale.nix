fc58c04038cff3e60f678ccbf477ef2f9b7f4753
Ilan Joselevich <personal@ilanjoselevich.com>
2023-07-29 23:55:11 +0300

    flake.lock: update,profiles: use nixos module

--- profiles/tailscale.nix
+++ profiles/tailscale.nix
@@ -1,47 +1,22 @@
 { inputs, ... }:
 
-{ pkgs, lib, config, ... }:
+{ pkgs, lib, config, options, ... }:
 
 {
   imports = [ inputs.agenix.nixosModules.age ];
 
-  services.tailscale.enable = true;
+  config = lib.mkMerge [
+    {
+      services.tailscale = {
+        enable = true;
+        useRoutingFeatures = "client";
+        authKeyFile = lib.mkDefault config.age.secrets.tailscaleAuthKey.path;
+      };
 
-  systemd.services.tailscaled = {
-    restartIfChanged = false;
-    requires = [ "tailscaled-autoconnect.service" ];
-    serviceConfig.ExecStart = [
-      ""
-      "${config.services.tailscale.package}/bin/tailscaled --state=mem: --port $PORT $FLAGS"
-    ];
-  };
-
-  age.secrets.tailscaleAuthKey.file = lib.mkDefault "${inputs.self}/secrets/infra-tailscaleAuthKey.age";
-
-  systemd.services.tailscaled-autoconnect = {
-    description = "Automatic connection to Tailscale";
-    after = [ "tailscaled.service" ];
-    requires = [ "tailscaled.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      ExecStart = lib.getExe (pkgs.writeShellScriptBin "tailscale-up" ''
-        # wait for tailscaled to settle
-        sleep 2
-
-        # check if we are already authenticated to tailscale
-        status="$(${lib.getExe config.services.tailscale.package} status -json | ${lib.getExe pkgs.jq} -r .BackendState)"
-        if [ $status = "Running" ]; then # if so, then do nothing
-          exit 0
-        fi
-
-        # otherwise authenticate with tailscale
-        ${lib.getExe config.services.tailscale.package} up \
-          --authkey file:${config.age.secrets.tailscaleAuthKey.path} \
-          --hostname=${config.networking.hostName}
-      '');
-      ExecStop = lib.getExe (pkgs.writeShellScriptBin "tailscale-logout" "${lib.getExe config.services.tailscale.package} logout || true");
-    };
-  };
+      age.secrets.tailscaleAuthKey.file = lib.mkDefault "${inputs.self}/secrets/infra-tailscaleAuthKey.age";
+    }
+    (lib.optionalAttrs (options ? environment.persistence) {
+      environment.persistence."/nix/persistent".directories = [ "/var/lib/tailscale" ];
+    })
+  ];
 }
