57b025b2092121e10eab1fc085db2c777b00f17f
Ilan Joselevich <personal@ilanjoselevich.com>
2023-08-17 22:51:04 +0300

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'agenix-rekey':
        'github:oddlama/agenix-rekey/dbe0dc1c41a9e8959baac6d7377d2051549ace2a' (2023-08-01)
      → 'github:oddlama/agenix-rekey/7e8f11a3cf6786477c420f2166a0c713bb706941' (2023-08-13)
    • Updated input 'disko':
        'github:nix-community/disko/241c878d4b542fea7c61ed4421e9224af054ff56' (2023-08-11)
      → 'github:nix-community/disko/4eed2457b053c4bbad7d90d2b3a1d539c2c9009c' (2023-08-16)
    • Updated input 'firefox-addons':
        'gitlab:rycee/nur-expressions/6f6a6a54586cf4a60577ade570d03c5bdd73b0ce?dir=pkgs/firefox-addons' (2023-08-08)
      → 'gitlab:rycee/nur-expressions/9088136d270494b3b0c8d39959da7d7facabd0ae?dir=pkgs/firefox-addons' (2023-08-17)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/6e1eff9aac0e8d84bda7f2d60ba6108eea9b7e79' (2023-08-10)
      → 'github:nix-community/home-manager/6a94c1a59737783c282c4031555a289c28b961e4' (2023-08-17)
    • Updated input 'neovim-nightly':
        'github:nix-community/neovim-nightly-overlay/dc3a7d8e8588ad320e7f3ef3e1f2aea20f0fe8df' (2023-08-11)
      → 'github:nix-community/neovim-nightly-overlay/39823353fd1f7bcb78670ae4cf0ec0f2e0cf4782' (2023-08-17)
    • Updated input 'neovim-nightly/neovim-flake':
        'github:neovim/neovim/3d948a4dc4b2cd3c8d3ac497caf3dfe25adfb90d?dir=contrib' (2023-08-10)
      → 'github:neovim/neovim/22d9338afceae5f8ef3845f152dea07a19d512d1?dir=contrib' (2023-08-16)
    • Updated input 'neovim-nightly/nixpkgs':
        'github:NixOS/nixpkgs/3d6ebeb283be256f008541ce2b089eb5fb0e4e01' (2023-08-09)
      → 'github:NixOS/nixpkgs/9b2aa98db6b10503666a50f4eb93b2fc0d57bde5' (2023-08-16)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/32c89128899f61681ed6a59ba7954eda3e70e95b' (2023-08-11)
      → 'github:NixOS/nixpkgs/c2f3ec724ca79b02b9aaf3b33ea8a47c47a424c4' (2023-08-17)

--- hosts/pongo/hosted/hercules-ci/default.nix
+++ hosts/pongo/hosted/hercules-ci/default.nix
@@ -3,8 +3,6 @@
 {
   services.hercules-ci-agent = {
     enable = true;
-    # I want 0.9.12 now!!!
-    package = (builtins.getFlake "github:NixOS/nixpkgs/23bb7c65c37945745c87ad9f002a428d51753b13").legacyPackages.${pkgs.system}.hercules-ci-agent;
     settings.binaryCachesPath = pkgs.writeText "binary-caches.json" "{}";
     settings.secretsJsonPath = config.age.secrets.herculesSecrets.path;
     settings.clusterJoinTokenPath = config.age.secrets.herculesClusterJoinToken.path;
