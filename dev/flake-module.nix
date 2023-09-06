ba547ab8fc0a767e2d51cb8068759dc56c8716a3
Ilan Joselevich <personal@ilanjoselevich.com>
2023-09-06 16:53:56 +0300

    dev: fix deploy script for the bazzilion time

--- dev/flake-module.nix
+++ dev/flake-module.nix
@@ -13,9 +13,9 @@
               TASK="switch"
             fi
             set -x
-            ${lib.optionalString cfg.config.security.sudo.wheelNeedsPassword "NIX_SSHOPTS=-tt"}
+            ${lib.optionalString cfg.config.security.sudo.wheelNeedsPassword "export NIX_SSHOPTS=-tt"}
             ${lib.getExe (pkgs.nixos-rebuild.override { nix = pkgs.nixUnstable; })} "$TASK" -s --use-remote-sudo --fast --flake ${inputs.self}#${host} \
-            --target-host ${cfg.config.networking.hostName} ${lib.optionalString (host == "pongo") "--build-host ${cfg.config.networking.hostName}"}
+              --target-host ${cfg.config.networking.hostName} ${lib.optionalString (host == "pongo") "--build-host ${cfg.config.networking.hostName}"}
           '');
         })
         inputs.self.nixosConfigurations)
