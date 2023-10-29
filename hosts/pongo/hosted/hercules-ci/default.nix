65dbb95319c6466b8249b288317db5906881c0e3
Ilan Joselevich <personal@ilanjoselevich.com>
2023-10-29 17:41:02 +0200

    WIP HCI

--- hosts/pongo/hosted/hercules-ci/default.nix
+++ hosts/pongo/hosted/hercules-ci/default.nix
@@ -1,23 +1,29 @@
 { pkgs, config, ... }:
 
 {
-  services.hercules-ci-agent = {
+
+  imports = [ ./multi ];
+
+  services.hercules-ci-agents."kranzes" = {
     enable = true;
-    settings.binaryCachesPath = pkgs.writeText "binary-caches.json" "{}";
-    settings.secretsJsonPath = config.age.secrets.herculesSecrets.path;
-    settings.clusterJoinTokenPath = config.age.secrets.herculesClusterJoinToken.path;
+    package = (builtins.getFlake "github:hercules-ci/hercules-ci-agent/e44538cf90ecd8173a6edf75f9a14364d3b9962f").packages.${pkgs.system}.hercules-ci-agent;
+    settings = {
+      binaryCachesPath = pkgs.writeText "binary-caches.json" "{}";
+      secretsJsonPath = config.age.secrets.herculesSecrets.path;
+      clusterJoinTokenPath = config.age.secrets.herculesClusterJoinToken.path;
+    };
   };
 
   age.secrets = {
     herculesSecrets = {
       rekeyFile = ./herculesSecrets.age;
-      group = "hercules-ci-agent";
-      owner = "hercules-ci-agent";
+      owner = config.services.hercules-ci-agents.kranzes.user;
+      inherit (config.services.hercules-ci-agents.kranzes) group;
     };
     herculesClusterJoinToken = {
       rekeyFile = ./${config.networking.hostName}-herculesClusterJoinToken.age;
-      group = "hercules-ci-agent";
-      owner = "hercules-ci-agent";
+      owner = config.services.hercules-ci-agents.kranzes.user;
+      inherit (config.services.hercules-ci-agents.kranzes) group;
     };
   };
 }
