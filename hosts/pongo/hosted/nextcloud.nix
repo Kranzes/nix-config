0d91cca13e9d42d503709685b23b3e7b0466e639
Ilan Joselevich <personal@ilanjoselevich.com>
2023-12-19 13:13:07 +0200

    hosts/pongo/hosted/nextcloud: 27 -> 28

--- hosts/pongo/hosted/nextcloud.nix
+++ hosts/pongo/hosted/nextcloud.nix
@@ -3,7 +3,7 @@
 {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud27;
+    package = pkgs.nextcloud28;
     hostName = "cloud.ilanjoselevich.com";
     nginx.recommendedHttpHeaders = true;
     https = true;
