e110aa4d9316f673053453885033155a7ae18122
Ilan Joselevich <personal@ilanjoselevich.com>
2024-01-29 22:36:16 +0200

    hosts/pongo/hosted/nextcloud: fix PKCE

--- hosts/pongo/hosted/nextcloud.nix
+++ hosts/pongo/hosted/nextcloud.nix
@@ -15,6 +15,7 @@
       adminpassFile = config.age.secrets.nextcloud-admin-root-pass.path;
       adminuser = "admin-root";
     };
+    extraOptions.user_oidc.use_pkce = true;
   };
 
   services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
