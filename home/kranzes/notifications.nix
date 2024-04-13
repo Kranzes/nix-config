aedf56c09d00f442b8956fabcd54bfb7c268775a
Ilan Joselevich <personal@ilanjoselevich.com>
2024-04-13 14:10:15 +0300

    home/kranzes/notifications: use catppuccin-nix

--- home/kranzes/notifications.nix
+++ home/kranzes/notifications.nix
@@ -3,31 +3,8 @@
 {
   services.dunst = {
     enable = true;
-    settings = {
-      global.font = "JetBrains Mono 10";
-
-      # catppuccin macchiato
-      global = {
-        frame_color = "#8AADF4";
-        separator_color = "frame";
-      };
-
-      urgency_low = {
-        background = "#24273A";
-        foreground = "#CAD3F5";
-      };
-
-      urgency_normal = {
-        background = "#24273A";
-        foreground = "#CAD3F5";
-      };
-
-      urgency_critical = {
-        background = "#24273A";
-        foreground = "#CAD3F5";
-        frame_color = "#F5A97F";
-      };
-    };
+    catppuccin.enable = true;
+    settings.global.font = "JetBrains Mono 10";
   };
 
   home.packages = [ pkgs.libnotify ];
