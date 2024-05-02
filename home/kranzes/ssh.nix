0f52321ba1a08aa36f6f83c54e48bbefa4e7b9d6
Ilan Joselevich <personal@ilanjoselevich.com>
2024-05-02 19:57:51 +0300

    home/kranzes/ssh: tvl gerrit setup

--- home/kranzes/ssh.nix
+++ home/kranzes/ssh.nix
@@ -2,5 +2,9 @@
   programs.ssh = {
     enable = true;
     hashKnownHosts = true;
+    matchBlocks."code.tvl.fyi" = {
+      identityFile = "~/.ssh/fuckgerrit";
+      port = 29418;
+    };
   };
 }
