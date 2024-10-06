a99d710e082dda273bb550e0ed3342097b2a7062
Ilan Joselevich <personal@ilanjoselevich.com>
2024-10-06 16:32:10 +0200

    profiles/laptop: add tzupdate

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -5,4 +5,7 @@
   # For WiFi.
   networking.networkmanager.enable = true;
   systemd.services.NetworkManager-wait-online.enable = false;
+
+  # Set the time zone automatically when travelling.
+  services.tzupdate.enable = true;
 }
