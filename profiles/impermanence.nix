37af6902d5bf7dd08289f5f27d31bc510e41d779
Ilan Joselevich <personal@ilanjoselevich.com>
2024-12-24 20:23:50 +0200

    {dev,profiles}: fix misc

--- profiles/impermanence.nix
+++ profiles/impermanence.nix
@@ -7,21 +7,18 @@ in
 {
   imports = [ inputs.impermanence.nixosModules.impermanence ];
 
-  config = lib.mkMerge [
-    {
-      environment.persistence."/nix/persistent" = {
-        hideMounts = true;
-        directories = [
-          "/var/lib/nixos"
-          "/var/log"
-          "/var/lib/systemd/coredump"
-          "/tmp" # Make builds not crash by running them on disk instead of RAM (We still clean /tmp on boot)
-        ];
-        files = [
-          "/etc/machine-id"
-        ] ++ sshHostKeys;
-      };
-    }
-    (lib.optionalAttrs (options ? age) { age.identityPaths = map (x: "/nix/persistent" + x) sshHostKeys; })
-  ];
+  config = {
+    environment.persistence."/nix/persistent" = {
+      hideMounts = true;
+      directories = [
+        "/var/lib/nixos"
+        "/var/log"
+        "/var/lib/systemd"
+        "/tmp" # Make builds not crash by running them on disk instead of RAM (We still clean /tmp on boot)
+      ];
+      files = [
+        "/etc/machine-id"
+      ] ++ sshHostKeys;
+    };
+  } // (lib.optionalAttrs (options ? age) { age.identityPaths = map (x: "/nix/persistent" + x) sshHostKeys; });
 }
