81ee0f7e1d682d768b3ac6a0640f7f4ab11a0c33
Ilan Joselevich <personal@ilanjoselevich.com>
2025-03-12 23:05:12 +0700

    fixup! hosts/pongo/xserver: don't fail if display is missing

--- hosts/pongo/xserver.nix
+++ hosts/pongo/xserver.nix
@@ -9,7 +9,7 @@
     windowManager.bspwm.enable = true;
     displayManager.setupCommands = ''
       ${pkgs.xorg.xrandr}/bin/xrandr --output DP-0 --rate 240 --mode 1920x1080 --primary || true
-      ${pkgs.xcalib}/bin/xcalib /home/kranzes/.xcalib/asus_rog_swift_pg258q.icc
+      ${pkgs.xcalib}/bin/xcalib /home/kranzes/.xcalib/asus_rog_swift_pg258q.icc || true
     '';
   };
 }
