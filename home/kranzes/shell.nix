21b9a03a383d28e38437c3c293fe900767a9ba31
Ilan Joselevich <personal@ilanjoselevich.com>
2025-04-24 21:01:44 +0300

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'catppuccin':
        'github:catppuccin/nix/592094a02c4e43a9fa33559ade84d1ca015e8ada' (2025-04-18)
      → 'github:catppuccin/nix/6268e50dbb0ac9375e110560395b5dc199e4dfb8' (2025-04-22)
    • Updated input 'disko':
        'github:nix-community/disko/51d33bbb7f1e74ba5f9d9a77357735149da99081' (2025-04-18)
      → 'github:nix-community/disko/ca27b88c88948d96feeee9ed814cbd34f53d0d70' (2025-04-24)
    • Updated input 'firefox-addons':
        'gitlab:rycee/nur-expressions/d53de206d64dd4581b5d5bbc7dcb2e8c5a1d836a?dir=pkgs/firefox-addons' (2025-04-18)
      → 'gitlab:rycee/nur-expressions/346899a4b3b651ab447c61e0c8e9d8a1454cda72?dir=pkgs/firefox-addons' (2025-04-23)
    • Updated input 'hercules-ci-agent':
        'github:hercules-ci/hercules-ci-agent/1d11bda7f6c4586a680e7504de77e93c82a5a713' (2025-04-09)
      → 'github:hercules-ci/hercules-ci-agent/575d191bc795161460963449b5c7f530f3a2015d' (2025-04-24)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/9676e8a52a177d80c8a42f66566362a6d74ecf78' (2025-04-19)
      → 'github:nix-community/home-manager/abfad3d2958c9e6300a883bd443512c55dfeb1be' (2025-04-24)
    • Updated input 'lanzaboote':
        'github:nix-community/lanzaboote/d8099586d9a84308ffedac07880e7f07a0180ff4' (2025-03-08)
      → 'github:nix-community/lanzaboote/995637eb3ab78eac33f8ee6b45cc2ecd5ede12ba' (2025-04-21)
    • Updated input 'lanzaboote/crane':
        'github:ipetkov/crane/75390a36cd0c2cdd5f1aafd8a9f827d7107f2e53' (2025-03-05)
      → 'github:ipetkov/crane/bb1c9567c43e4434f54e9481eb4b8e8e0d50f0b5' (2025-03-09)
    • Updated input 'lanzaboote/flake-parts':
        'github:hercules-ci/flake-parts/3876f6b87db82f33775b1ef5ea343986105db764' (2025-03-01)
      → 'github:hercules-ci/flake-parts/f4330d22f1c5d2ba72d3d22df5597d123fdb60a9' (2025-03-07)
    • Updated input 'lanzaboote/nixpkgs':
        'github:NixOS/nixpkgs/ffe8d1b1030b5de6eba761102ee34b6e41d040ee' (2025-03-06)
      → 'github:NixOS/nixpkgs/8adda98a4e637d0d8a867f19ace5ed6680ecc94a' (2025-03-09)
    • Updated input 'lanzaboote/pre-commit-hooks-nix':
        'github:cachix/pre-commit-hooks.nix/42b1ba089d2034d910566bf6b40830af6b8ec732' (2025-03-02)
      → 'github:cachix/pre-commit-hooks.nix/b5a62751225b2f62ff3147d0a334055ebadcd5cc' (2025-03-07)
    • Updated input 'lanzaboote/rust-overlay':
        'github:oxalica/rust-overlay/38e9826bc4296c9daf18bc1e6aa299f3e932a403' (2025-03-06)
      → 'github:oxalica/rust-overlay/c777dc8a1e35407b0e80ec89817fe69970f4e81a' (2025-03-10)
    • Updated input 'lix':
        'git+https://git.lix.systems/lix-project/lix.git?ref=refs/heads/main&rev=81d476984901d1f00fdcac36a0a8f06cb54048f6' (2025-04-10)
      → 'git+https://git.lix.systems/lix-project/lix.git?ref=refs/heads/main&rev=c36c83c4c6ceb6166cf4a6867354d7244a0c7984' (2025-04-24)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/b024ced1aac25639f8ca8fdfc2f8c4fbd66c48ef' (2025-04-17)
      → 'github:NixOS/nixpkgs/8a2f738d9d1f1d986b5a4cd2fd2061a7127237d7' (2025-04-23)
    • Updated input 'srvos':
        'github:nix-community/srvos/c6808c5b6575ae3343dc3c21277080f68a574bd6' (2025-04-12)
      → 'github:nix-community/srvos/be128093568b7f0d6d9d33941564e78d19bf7c06' (2025-04-22)

--- home/kranzes/shell.nix
+++ home/kranzes/shell.nix
@@ -16,35 +16,37 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    initExtraFirst = ''
-      setopt PROMPT_SUBST
-      source ${pkgs.git}/share/bash-completion/completions/git-prompt.sh
-      PROMPT='[%n@%m %~]%(!.#.$)$(__git_ps1) '
-      RPROMPT=
-    '';
-    initExtraBeforeCompInit = ''
-      export LS_COLORS="$(${lib.getExe pkgs.vivid} generate catppuccin-${config.catppuccin.flavor})"
-    '';
-    initExtra = ''
-      setopt INTERACTIVE_COMMENTS
-      bindkey "^R" history-incremental-search-backward
-      bindkey "^A" vi-beginning-of-line
-      bindkey "^E" vi-end-of-line
-      # Disable pasted text highlighting 
-      zle_highlight+=(paste:none)
-      # Completion options
-      zstyle ':completion:*:default' list-colors ''${(s.:.)LS_COLORS}
-      zstyle ':completion:*' menu select
-      zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
-      zstyle ':completion:*:descriptions' format ' %F{yellow}-- %d --%f'
-      zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
-      zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
-      zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
-      zstyle ':completion:*' group-name ""
-      # Quote pasted URLs
-      autoload -U url-quote-magic
-      zle -N self-insert url-quote-magic
-    '';
+    initContent = lib.mkMerge [
+      (lib.mkBefore ''
+        setopt PROMPT_SUBST
+        source ${pkgs.git}/share/bash-completion/completions/git-prompt.sh
+        PROMPT='[%n@%m %~]%(!.#.$)$(__git_ps1) '
+        RPROMPT=
+      '')
+      (lib.mkOrder 550 ''
+        export LS_COLORS="$(${lib.getExe pkgs.vivid} generate catppuccin-${config.catppuccin.flavor})"
+      '')
+      ''
+        setopt INTERACTIVE_COMMENTS
+        bindkey "^R" history-incremental-search-backward
+        bindkey "^A" vi-beginning-of-line
+        bindkey "^E" vi-end-of-line
+        # Disable pasted text highlighting 
+        zle_highlight+=(paste:none)
+        # Completion options
+        zstyle ':completion:*:default' list-colors ''${(s.:.)LS_COLORS}
+        zstyle ':completion:*' menu select
+        zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
+        zstyle ':completion:*:descriptions' format ' %F{yellow}-- %d --%f'
+        zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+        zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+        zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
+        zstyle ':completion:*' group-name ""
+        # Quote pasted URLs
+        autoload -U url-quote-magic
+        zle -N self-insert url-quote-magic
+      ''
+    ];
     shellAliases = {
       history = "history 1";
       rm = "rm -I";
