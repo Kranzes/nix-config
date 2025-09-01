dabc3330cd95d782232a341040d9ced2c90e23c4
Ilan Joselevich <personal@ilanjoselevich.com>
2025-09-01 19:45:17 +0300

    hosts/pongo/xserver: enable jetkvm hdmi output

--- hosts/pongo/xserver.nix
+++ hosts/pongo/xserver.nix
@@ -6,6 +6,7 @@
     windowManager.bspwm.enable = true;
     displayManager.setupCommands = ''
       ${pkgs.xorg.xrandr}/bin/xrandr --output DisplayPort-0 --rate 240 --mode 1920x1080 --primary
+      ${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-A-0 --mode 1920x1080 --same-as DisplayPort-0
       ${pkgs.xcalib}/bin/xcalib /home/kranzes/.xcalib/asus_rog_swift_pg258q.icc || true
     '';
   };
