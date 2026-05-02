0e5d4110875af28560fca0503500b4c0ebd5d751
Ilan Joselevich <personal@ilanjoselevich.com>
2026-05-03 01:47:56 +0300

    home/kranzes/terminal: pad alacritty window

--- home/kranzes/terminal.nix
+++ home/kranzes/terminal.nix
@@ -6,6 +6,10 @@
         normal.family = "JetBrainsMonoNL Nerd Font";
         size = 9;
       };
+      window.padding = {
+        x = 3;
+        y = 3;
+      };
       keyboard.bindings = [
         {
           key = "Return";
