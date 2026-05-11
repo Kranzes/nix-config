89ff5ab2155832bd34ec4d422250d0bb9348b01a
Ilan Joselevich <personal@ilanjoselevich.com>
2026-05-11 14:56:28 +0300

    profiles/nix-nixpkgs: add garnix binary cache as substituter

--- profiles/nix-nixpkgs.nix
+++ profiles/nix-nixpkgs.nix
@@ -35,9 +35,11 @@
       max-substitution-jobs = 128;
       substituters = [
         "https://nix-community.cachix.org"
+        "https://cache.garnix.io"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.garnix.io:CTFPyKSLcx5RMAX6qNkq3x9PvVdHzx7IjVwZoonPKWU="
       ];
     };
   };
