bd43de9d3a3a7ca5d0fb39a52d12462fa351d234
Ilan Joselevich <personal@ilanjoselevich.com>
2026-05-15 16:29:29 +0300

    hosts/tamarin/home/ssh-tpm-agent: fix

--- hosts/tamarin/home/ssh-tpm-agent.nix
+++ hosts/tamarin/home/ssh-tpm-agent.nix
@@ -1,14 +1,23 @@
-{ lib, config, ... }:
 {
-  services.ssh-tpm-agent.enable = true;
-
-  systemd.user.services.ssh-tpm-agent.Service = {
-    ExecStart = lib.mkForce [
-      ""
-      "${lib.getExe config.services.ssh-tpm-agent.package} --confirm-loaded"
-    ];
-    Environment = [
-      "SSH_ASKPASS=${config.home.sessionVariables.SSH_ASKPASS}"
-    ];
+  pkgs,
+  config,
+  ...
+}:
+{
+  services.ssh-tpm-agent = {
+    enable = true;
+    package = pkgs.ssh-tpm-agent.overrideAttrs {
+      patches = [
+        (pkgs.fetchpatch {
+          url = "https://github.com/Foxboron/ssh-tpm-agent/commit/b49c37f6dd11781d0c0cb696ac0bd37d1f353a49.patch";
+          hash = "sha256-QBZTclT3UZMKGJ9YKF67r03tOpRonJ/ww9tGePpjJmM=";
+        })
+      ];
+    };
+    extraArgs = [ "--confirm-loaded" ];
   };
+
+  systemd.user.services.ssh-tpm-agent.Service.Environment = [
+    "SSH_ASKPASS=${config.home.sessionVariables.SSH_ASKPASS}"
+  ];
 }
