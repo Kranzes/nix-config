0798e9be7a60c748621c5bf4cf45d2ab401c117e
Ilan Joselevich <personal@ilanjoselevich.com>
2026-05-18 03:03:59 +0300

    profiles/nix-nixpkgs: update cache.garnix.io trusted public key

--- profiles/nix-nixpkgs.nix
+++ profiles/nix-nixpkgs.nix
@@ -39,7 +39,7 @@
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.garnix.io:CTFPyKSLcx5RMAX6qNkq3x9PvVdHzx7IjVwZoonPKWU="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
   };
