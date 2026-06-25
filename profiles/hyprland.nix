6809c0bd608477aaedc66372b610ebdb7907ac79
Ilan Joselevich <personal@ilanjoselevich.com>
2026-06-25 23:41:39 +0300

    profiles/hyprland: patch Hyprland for keyboard refocus on panel close

--- profiles/hyprland.nix
+++ profiles/hyprland.nix
@@ -1,7 +1,18 @@
+{ pkgs, ... }:
 {
   programs.hyprland = {
     enable = true;
     withUWSM = true;
+    package = pkgs.hyprland.overrideAttrs (old: {
+      patches = (old.patches or [ ]) ++ [
+        (pkgs.fetchpatch {
+          name = "hyprland-refocus.patch";
+          url = "https://github.com/hyprwm/Hyprland/commit/ab718d8ae71875804222f5ad3f80d8f2cd592330.patch";
+          includes = [ "src/desktop/state/FocusState.cpp" ];
+          hash = "sha256-Un728vGoK1vCnBf4BKE8jeSgR0b8Ad/dO+/tHuu02n8=";
+        })
+      ];
+    });
   };
 
   programs.regreet.enable = true;
